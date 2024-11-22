fb48e71401b31e8fc6dd06569b0dadee3d9daec2
Пользователь <dimaschelichov228@gmail.com>
2024-11-22 15:51:17 +0100

    Add CD/CI

--- test_authentication.py
+++ test_authentication.py
@@ -6,7 +6,6 @@ import unittest
 class TestAuthentication(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Настроим драйвер Chrome
         cls.driver = webdriver.Chrome()
         cls.driver.maximize_window()
         
@@ -14,15 +13,15 @@ class TestAuthentication(unittest.TestCase):
         driver = self.driver
         driver.get('http://localhost:3000/login')
         
-        # Заполнение формы логина
+        
         driver.find_element(By.NAME, 'email').send_keys('john.doe@gmail.com')
         driver.find_element(By.NAME, 'password').send_keys('SecurePassword_123')
         driver.find_element(By.CSS_SELECTOR, '.form-button').click()
         
-        # Ожидаем загрузки страницы
-        time.sleep(2)  # Можно заменить на явное ожидание, если нужно
+        
+        time.sleep(2) 
 
-        # Проверяем URL после успешного входа
+        
         current_url = driver.current_url
         print(current_url)
         self.assertEqual(current_url, 'http://localhost:3000/profile')
@@ -31,7 +30,7 @@ class TestAuthentication(unittest.TestCase):
         driver = self.driver
         driver.get('http://localhost:3000/register')
         
-        # Заполнение формы регистрации
+        
         driver.find_element(By.NAME, 'firstName').send_keys('John')
         driver.find_element(By.NAME, 'lastName').send_keys('Doe')
         driver.find_element(By.NAME, 'email').send_keys('test@example.com')
@@ -40,17 +39,16 @@ class TestAuthentication(unittest.TestCase):
         driver.find_element(By.NAME, 'passwordRepeat').send_keys('SecurePassword_123')
         driver.find_element(By.CSS_SELECTOR, '.form-button').click()
         
-        # Ожидаем загрузки страницы
-        time.sleep(2)  # Можно заменить на явное ожидание, если нужно
+      
+        time.sleep(2) 
 
-        # Проверяем URL после успешной регистрации
         current_url = driver.current_url
         print(current_url)
         self.assertEqual(current_url, 'http://localhost:3000/profile')
 
     @classmethod
     def tearDownClass(cls):
-        # Закрываем драйвер
+      
         cls.driver.quit()
 
 if __name__ == '__main__':
